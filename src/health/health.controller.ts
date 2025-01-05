da309e6d4562134ec4d7ed2f67b726d027a733c1
ahmedfaiyaz-6sense <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2025-01-05 23:53:42 +0600

    removed storage check from terminus
    Will add this later.

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -6,7 +6,7 @@ import {
   HttpHealthIndicator,
 } from '@nestjs/terminus';
 import { PrismaHealthIndicator } from '@nestjs/terminus';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 @Controller('health')
 export class HealthController {
   constructor(
@@ -23,8 +23,6 @@ export class HealthController {
     return this.health.check([
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com/'),
       () => this.http.pingCheck('sentry-endpoint', 'https://sentry.io'),
-      () =>
-        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.8 }),
       () => this.prismaorm.pingCheck('Mongodb-check', this.prismaService),
     ]);
   }
